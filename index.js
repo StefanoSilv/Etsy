a5bbade39f8f4668f5f3cf45b357ed7c350c06a6
Stefano Silvestrini <macbookpro@MacBook-Pro-di-MacBook.local>
2019-06-18 12:14:31 +0200

    The HTML file completed and the server is running - with the new files tracked

---
+++ index.js
@@ -0,0 +1,13 @@
+const express= require('express')
+const path = require('path')
+const app= express()
+
+app.use(express.static(path.join(__dirname, 'client')))
+
+app.get('/marketshop', (req, res)=>{
+	res.sendFile(path.join(__dirname, './client/marketshop.html'))
+})
+
+app.listen(3100, ()=>{
+	console.log('Server listening on port 3100');
+})
